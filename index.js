e65fb4d657c7f80acd0b74ec8bf39442b96c0b84
xuebin <xuebin7@163.com>
2025-10-20 17:44:57 +0800

    更新2025-10-17数据

--- index.js
+++ index.js
@@ -9037,6 +9037,7 @@ let statData = [
   { date: "2025-10-14", vix: 20.81 },
   { date: "2025-10-15", vix: 20.64 },
   { date: "2025-10-16", vix: 25.31 },
+  { date: "2025-10-17", vix: 20.78 },
 ];
 
 const chartDom = document.getElementById("chart");
